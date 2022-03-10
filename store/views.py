aa263fd792fa7ff3220f86ab1c7053609bd57b5a
Hacho55 <horacioesuarez@gmail.com>
2022-03-10 20:55:07 -0300

    Before filtering products

--- store/views.py
+++ store/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Collection, OrderItem, Product
-from .serializers import ProductSerializer, CollectionSerializer
+from .models import Collection, OrderItem, Product, Review
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from rest_framework import status
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 
@@ -36,6 +36,17 @@ class CollectionViewSet(ModelViewSet):
         if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Collection cannot be deleted because it has products asociated'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
+
+
+class ReviewViewSet(ModelViewSet):
+
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
 
 
 # @api_view(['GET', 'PUT', 'DELETE'])
